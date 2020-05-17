881631d9263cdc99ea6a0df3a6a34fee129a463f
AnthonynTilelli <anthony@tilelli.me>
2020-05-16 19:23:07 -0700

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,6 @@ Classroom.create(course_name: 'English', semester: "Fall #{Time.now.year}")
 Classroom.create(course_name: 'Science', semester: "Spring #{Time.now.year}")
 Classroom.create(course_name: 'Chemistry', semester: "Fall #{Time.now.year}")
 
-
 Classroom.all.each_with_index do |classroom, i|
-  classroom.students << [Student.limit(8).offset(i*2)]
+  classroom.students << [Student.limit(8).offset(i * 2)]
 end
