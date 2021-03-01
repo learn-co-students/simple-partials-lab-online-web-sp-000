e76a475f4fbf1980a7d6b3e43fc70e07379ff3cb
nlanese21 <nlanese21@gmail.com>
2021-03-01 01:55:06 -0500

    Done.

---
+++ spec/views/tests/edit.html.erb_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "tests/edit", type: :view do
+  before(:each) do
+    @test = assign(:test, Test.create!(
+      var: "MyString"
+    ))
+  end
+
+  it "renders the edit test form" do
+    render
+
+    assert_select "form[action=?][method=?]", test_path(@test), "post" do
+
+      assert_select "input#test_var[name=?]", "test[var]"
+    end
+  end
+end
